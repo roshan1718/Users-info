1a247a8f930cd430024b5c746c10d7be40b21068
roshan1718 <roshanshinde1717@gmail.com>
2021-10-08 16:41:17 +0530

    Add pages and apply routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-
+import Home from './components/pages/Home'
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import { Navbar } from './components/layout/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
+  <Router>
     <div className="App">
-     <h1>hiii</h1>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} />
+
+      </Switch>
     </div>
+  </Router>
   );
 }
 
